2afb6f5b0d72056bf02fb6d8a6b5bf43391cc56b
Juan Jose Estrella Ramirez <juestrel@c2r6s5.42malaga.com>
2023-12-18 12:50:18 +0100

    Initial commit

---
+++ get_next_line.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juestrel <juestrel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/18 12:45:22 by juestrel          #+#    #+#             */
+/*   Updated: 2023/12/18 12:49:17 by juestrel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+#endif
